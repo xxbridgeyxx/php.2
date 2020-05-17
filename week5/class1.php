89b926c521d1290388e362355e893e9b9dec939d
Bridge Mikaera <mikaeraboo@Bridges-MacBook-Air.local>
2020-05-18 10:26:37 +1200

    something

--- week5/class1.php
+++ week5/class1.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="css/styleclass1.css">
 <?php
 
 echo "helloworld";
@@ -8,8 +9,7 @@ echo "<p>Hello World</p>";
 
 echo "<p style='color: mediumslateblue'>Hello World</p>";
 
-https://bridgeyxxxx.herokuapp.com
-
+//https://bridgeyxxxx.herokuapp.com
 $i = 0; // set counter
 while ($i +100){ // set condition
     echo "<p>Hello</p>"; // do something if condition is true
