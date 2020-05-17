50f8ba15175e38b69d0eff730252c0d180f1bdab
Bridge Mikaera <mikaeraboo@Bridges-MacBook-Air.local>
2020-05-18 10:27:53 +1200

    something

--- week5/class1.php
+++ week5/class1.php
@@ -11,7 +11,7 @@ echo "<p style='color: mediumslateblue'>Hello World</p>";
 
 //https://bridgeyxxxx.herokuapp.com
 $i = 0; // set counter
-while ($i +100){ // set condition
+while ($i < 100){ // set condition
     echo "<p>Hello</p>"; // do something if condition is true
     $i = $i + 1; //update counter
 }
