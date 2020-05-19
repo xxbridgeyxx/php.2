86c6a8ae8b2b4e000a89f47af76d8173cf8da0ba
Bridge Mikaera <mikaeraboo@Bridges-MacBook-Air.local>
2020-05-20 09:38:28 +1200

    something

--- week5/calculate.php
+++ week5/calculate.php
@@ -3,7 +3,7 @@ if (isset($_POST["width"])){
     $w = $_POST["width"];
     $h = $_POST["height"];
     $area = $w * $h;
-    echo "area: ".$area;
+    echo "Area: ".$area;
 }else{
     echo "go back to form page";
 }
