ec626822d5510bca9f3bfd20a837b692348b371f
Bridge Mikaera <mikaeraboo@Bridges-MacBook-Air.local>
2020-06-04 09:25:54 +1200

    php

--- Week6/showrecords.php
+++ Week6/showrecords.php
@@ -8,7 +8,11 @@ while ($i < sizeof($records)){
 //    print_r($records[$i]); //arrayname[index] --> value
     $record = $records[$i]; //inside each record, we have place 0 for id, place 1 for username, place 2 for password, place 3 for name
     ?>
-    <p><?php echo $record[0]; ?> <?php echo $record[1]; ?> <?php echo $record[2]; ?> <?php echo $record[3]; ?></p>
+    <p><?php echo $record->id; ?>
+        <?php echo $record->username; ?>
+        <?php echo $record->password; ?>
+        <?php echo $record->name; ?>
+    </p>
     <?php
     $i = $i + 1;
 }
